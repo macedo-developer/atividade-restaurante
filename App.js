079e3afbca0f2aa0c70372a7aa9ddbd932b8953f
macedo-developer <macedorenata.developer@gmail.com>
2020-10-10 23:34:31 -0300

    Remove variable not in use

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-import { Button, Card, TextInput } from "react-native-paper";
+import { Button, TextInput } from "react-native-paper";
 
 export default class App extends React.Component {
   state = {
